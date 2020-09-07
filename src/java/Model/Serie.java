a768cafae4d02b9ee5e73087b8f73c906149279f
User <User@ArielEsteban>
2020-09-06 21:33:26 -0500

    Agregar Modelo de Serie

---
+++ src/java/Model/Serie.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author User
+ */
+public class Serie {
+   private int id;
+   private String Nombre;
+   private String Ano;
+   private int CantidadTemporadas;
+   private String Descripcion;
+   private int IdGenero;
+   private int IdTemporada;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getAno() {
+        return Ano;
+    }
+
+    public void setAno(String Ano) {
+        this.Ano = Ano;
+    }
+
+    public int getCantidadTemporadas() {
+        return CantidadTemporadas;
+    }
+
+    public void setCantidadTemporadas(int CantidadTemporadas) {
+        this.CantidadTemporadas = CantidadTemporadas;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    public int getIdGenero() {
+        return IdGenero;
+    }
+
+    public void setIdGenero(int IdGenero) {
+        this.IdGenero = IdGenero;
+    }
+
+    public int getIdTemporada() {
+        return IdTemporada;
+    }
+
+    public void setIdTemporada(int IdTemporada) {
+        this.IdTemporada = IdTemporada;
+    }
+   
+}
