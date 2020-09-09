ef8d524cc6ec88c8e435e4bbff01131378bca074
Usuario <Usuario@Santiago.mshome.net>
2020-09-08 23:55:05 -0500

    urls y paginas

--- src/java/Controller/PeliculaController.java
+++ src/java/Controller/PeliculaController.java
@@ -23,50 +23,50 @@ public class PeliculaController {
     int id;
     List datos;
     
-    @RequestMapping ("indexPelicula.htm")
+    @RequestMapping ("indexPeliculaspelis.htm")
     public ModelAndView Listar(){
     datos=peliculaBL.listar();
     mav.addObject("lsPelicula",datos);
-    mav.setViewName("indexPelicula");
+    mav.setViewName("indexPeliculaspelis");
     return mav;
     }
     
-    @RequestMapping(value="agregarPelicula.htm",method=RequestMethod.GET)
+    @RequestMapping(value="agregarPeliculaspelis.htm",method=RequestMethod.GET)
     public ModelAndView Agregar(){
         mav.addObject(new Pelicula());
         datos = generoBL.listar();
         mav.addObject("lsGeneros", datos);
-        mav.setViewName("agregarPelicula");
+        mav.setViewName("agregarPeliculaspelis");
         return mav;
     } 
-    @RequestMapping(value="agregarPelicula.htm",method=RequestMethod.POST)
+    @RequestMapping(value="agregarPeliculaspelis.htm",method=RequestMethod.POST)
     public ModelAndView Agregar(Pelicula p){        
         peliculaBL.insertar(p);
-        return new ModelAndView("redirect:/indexPelicula.htm");
+        return new ModelAndView("redirect:/indexPeliculaspelis.htm");
     }
     
-    @RequestMapping(value="editarPelicula.htm",method=RequestMethod.GET)
+    @RequestMapping(value="editarPeliculaspelis.htm",method=RequestMethod.GET)
     public ModelAndView Editar(HttpServletRequest request){
         id=Integer.parseInt(request.getParameter("id"));
         datos=peliculaBL.buscar(id);
-        mav.addObject("editPelicula", datos);
+        mav.addObject("editPeliculaspelis", datos);
         datos=generoBL.listar();
         mav.addObject("lsGeneros", datos);
-        mav.setViewName("editarPelicula");
+        mav.setViewName("editarPeliculaspelis");
         return mav;
     }
     
-     @RequestMapping(value="editarPelicula.htm",method=RequestMethod.POST)
+     @RequestMapping(value="editarPeliculaspelis.htm",method=RequestMethod.POST)
     public ModelAndView Editar(Pelicula p)
     {
         peliculaBL.actualizar(p);
-        return new ModelAndView("redirect:/indexPelicula.htm");
+        return new ModelAndView("redirect:/indexPeliculaspelis.htm");
     }
     
-    @RequestMapping("deletePelicula.htm")
+    @RequestMapping("deletePeliculaspelis.htm")
     public ModelAndView Delete(HttpServletRequest request){
         id=Integer.parseInt(request.getParameter("id"));
         peliculaBL.eliminar(id);
-        return new ModelAndView("redirect:/indexPelicula.htm"); 
+        return new ModelAndView("redirect:/indexPeliculaspelis.htm"); 
     }  
 }
